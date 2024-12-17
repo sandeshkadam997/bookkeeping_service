65e045a3b37598cb701d85470c10a59baf1dfcdd
Sandesh Kadam <sandeshkadam997@gmail.com>
2024-12-18 00:55:17 +0530

    Code Review

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const db = require('./db');
 require('dotenv').config();
 const {jwtAuthMiddleware} = require('./jwt');
 
-// const bodyParser = require('body-parser'); 
-// app.use(bodyParser.json()); 
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send('Welcome to Book Keeping Service');
+})
 
 const userRoutes = require('./routes/userRoutes');
 const bookRoutes = require('./routes/bookRoutes');
@@ -19,7 +21,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/books',jwtAuthMiddleware, bookRoutes);
 app.use('/api/libraries',jwtAuthMiddleware, libraryRoutes);
 app.use('/api/borrow',jwtAuthMiddleware, borrowRoutes);
-app.use('/api/libraries',jwtAuthMiddleware, libraryInventoryRoutes);
+app.use('/api/libraries/:id/inventory',jwtAuthMiddleware, libraryInventoryRoutes);
 
 app.listen(PORT, ()=>{
     console.log('listening on port 3000');
